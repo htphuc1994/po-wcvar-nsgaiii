7b121b1bc4e89774e284bc4f077f92ac1973951f
htphuc.cs <htphuc.cs@gmail.com>
2024-07-03 17:24:14 +0700

    add-experiment-11

--- extract_data.py
+++ extract_data.py
@@ -1,4 +1,4 @@
-input_file = 'output/output-03072024164744.txt'  # Replace with your input file path
+input_file = 'output/output-03072024170547.txt'  # Replace with your input file path
 output_file = 'output/out-refined.txt'  # Replace with your output file path
 patterns = ['len(fronts)', '========', 'n_gen  |  n_eval', '     ']  # The pattern to look for
 
