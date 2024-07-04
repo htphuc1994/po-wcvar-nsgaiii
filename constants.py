c9b07f8f902daa9916bf0b42b7319f152539a899
htphuc.cs <htphuc.cs@gmail.com>
2024-07-04 11:48:40 +0700

    add-experiment-12

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 import math
 
-INITIAL_CASH = 100000  # 1 BLN VND # NOTE if change plz update the same variable at nsga3.py as well, because they are two seperate projects
-DURATION = 12  # 6 MONTHS # NOTE if change plz update the same variable at nsga3.py as well, because they are two seperate projects
-WAVELET_LEVEL = 3  # 12, 6, 4 months => 3, 2, 1 todo change me if change DURATION
+INITIAL_CASH = 10000000  # 1 BLN VND # NOTE if change plz update the same variable at nsga3.py as well, because they are two seperate projects
+DURATION = 6  # 6 MONTHS # NOTE if change plz update the same variable at nsga3.py as well, because they are two seperate projects
+WAVELET_LEVEL = 2  # 12, 6, 4 months => 3, 2, 1 todo change me if change DURATION
 
 
 BANK_INTEREST_RATE = 0.0045  # NOTE if change plz update the same variable at nsga3.py as well, because they are two seperate projects
