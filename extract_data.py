b910dbb2f46b01d2e4ab8b4ac077aae9846b2718
htphuc.cs <htphuc.cs@gmail.com>
2024-07-06 19:29:51 +0700

    add-experiments

--- extract_data.py
+++ extract_data.py
@@ -1,8 +1,18 @@
-input_file = 'output/output-05072024164314.txt'  # Replace with your input file path
-output_file = 'output/out-refined.txt'  # Replace with your output file path
+input_file = 'output/output-06072024065058.txt'  # Replace with your input file path
+output_file = 'output/experiment_.txt'  # Replace with your output file path
 patterns = ['len(fronts)', '========', 'n_gen  |  n_eval', '     ']  # The pattern to look for
 
+
+
 with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    for line in infile:
+    lines = infile.readlines()
+    last_two_lines = lines[-2:]
+    # outfile.writelines(last_two_lines)
+
+    lines_to_write = last_two_lines.copy()
+
+    for line in lines:
         if any(pattern in line for pattern in patterns):
-            outfile.write(line)
+            lines_to_write.append(line)
+            # outfile.write(line)
+    outfile.writelines(lines_to_write)
