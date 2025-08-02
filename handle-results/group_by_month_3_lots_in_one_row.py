199837f46ca2cf7c5ecd1c1fc050de7cf22c7f35
htphuc.cs <htphuc.cs@gmail.com>
2025-08-02 20:25:52 +0700

    standard-all-charts

--- handle-results/group_by_month_3_lots_in_one_row.py
+++ handle-results/group_by_month_3_lots_in_one_row.py
@@ -10,7 +10,7 @@ import numpy as np
 # year_returns = [0.0553567, 0.20202, 0.29067, 0.26279]
 
 # categories = ['A', 'B', 'C', 'D', 'E']
-categories = ['Bank deposit', 'VND100 million', 'VND1 billion', 'VND10 billion']
+categories = ['Bank Deposit', 'VND100 Million', 'VND1 Billion', 'VND10 Billion']
 quarter_returns = [0.01812, 0.1335135256481, 0.0895296482672, 0.0667413163178]  # Generate random values between 0 and 1 for the columns
 # half year
 half_year_returns = [0.0273055, 0.2351092913214, 0.2854574844757, 0.2550369885965]
@@ -34,38 +34,38 @@ fig, axs = plt.subplots(1, 3, figsize=(18, 6))
 bars1 = axs[0].bar(categories, quarter_returns, color=colors, label=categories)
 for bar, hatch in zip(bars1, hatch_patterns):
     bar.set_hatch(hatch)
-axs[0].set_title('3-month investment', fontsize=16)
-axs[0].set_xticklabels(categories, fontsize=16)
+axs[0].set_title('3-month Investment', fontsize=24)
+axs[0].set_xticklabels(categories, fontsize=24)
 axs[0].set_xticks([])
-axs[0].set_ylim(0, 100)
-axs[0].set_ylabel('Return (%)', fontsize=16)
-axs[0].legend(fontsize=16)
+axs[0].set_ylim(0, 60)
+axs[0].set_ylabel('Return (%)', fontsize=24)
+axs[0].legend(fontsize=24)
 axs[0].grid(True)
 
 bars2 = axs[1].bar(categories, half_year_returns, color=colors, label=categories)
 for bar, hatch in zip(bars2, hatch_patterns):
     bar.set_hatch(hatch)
-axs[1].set_title('6-month investment', fontsize=16)
-axs[1].set_xticklabels(categories, fontsize=16)
+axs[1].set_title('6-month Investment', fontsize=24)
+axs[1].set_xticklabels(categories, fontsize=24)
 axs[1].set_xticks([])
-axs[1].set_ylabel('Return (%)', fontsize=16)
-axs[1].set_ylim(0, 100)
-axs[1].legend(fontsize=16)
+axs[1].set_ylabel('Return (%)', fontsize=24)
+axs[1].set_ylim(0, 60)
+axs[1].legend(fontsize=24)
 axs[1].grid(True)
 
 bars3 = axs[2].bar(categories, year_returns, color=colors, label=categories)
 for bar, hatch in zip(bars3, hatch_patterns):
     bar.set_hatch(hatch)
-axs[2].set_title('12-month investment', fontsize=16)
-axs[2].set_xticklabels(categories, fontsize=16)
+axs[2].set_title('12-month Investment', fontsize=24)
+axs[2].set_xticklabels(categories, fontsize=24)
 axs[2].set_xticks([])
-axs[2].set_ylim(0, 100)
-axs[2].set_ylabel('Return (%)', fontsize=16)
-axs[2].legend(fontsize=16)
+axs[2].set_ylim(0, 60)
+axs[2].set_ylabel('Return (%)', fontsize=24)
+axs[2].legend(fontsize=24)
 axs[2].grid(True)
 
 for ax in axs:
-    ax.tick_params(axis='y', labelsize=16)
+    ax.tick_params(axis='y', labelsize=24)
 
 # Adjust the layout to prevent overlap
 plt.tight_layout()
